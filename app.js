a1bd63533d67a5d6f1f969cf9711283a0a262a0f
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-09-13 22:20:12 -0300

    Initiating css structure

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { cardArray } from "./cardArray.js";
 const gridDisplay = document.querySelector("#grid");
 const resultDisplay = document.querySelector("#result");
 let cards
@@ -5,76 +6,8 @@ let cardsChosenNames = [];
 let cardsChosenIds = [];
 const cardsWon = [];
 
-const cardArray = [
-    {
-        name: 'blouse1',
-        img: 'images/blouse1.jpg',
-    },
-    {
-        name: 'blouse2',
-        img: 'images/blouse2.jpg',
-    },
-    {
-        name: 'blouse3',
-        img: 'images/blouse3.jpg',
-    },
-    {
-        name: 'blouse4',
-        img: 'images/blouse4.jpg',
-    },
-    {
-        name: 'blouse5',
-        img: 'images/blouse5.jpg',
-    },
-    {
-        name: 'blouse6',
-        img: 'images/blouse6.jpg',
-    },
-    {
-        name: 'blouse7',
-        img: 'images/blouse7.jpg',
-    },
-    {
-        name: 'blouse8',
-        img: 'images/blouse8.jpg',
-    },
-    {
-        name: 'blouse1',
-        img: 'images/blouse1.jpg',
-    },
-    {
-        name: 'blouse2',
-        img: 'images/blouse2.jpg',
-    },
-    {
-        name: 'blouse3',
-        img: 'images/blouse3.jpg',
-    },
-    {
-        name: 'blouse4',
-        img: 'images/blouse4.jpg',
-    },
-    {
-        name: 'blouse5',
-        img: 'images/blouse5.jpg',
-    },
-    {
-        name: 'blouse6',
-        img: 'images/blouse6.jpg',
-    },
-    {
-        name: 'blouse7',
-        img: 'images/blouse7.jpg',
-    },
-    {
-        name: 'blouse8',
-        img: 'images/blouse8.jpg',
-    }
-];
-
+// Sorting the cards
 cardArray.sort(() => 0.5 - Math.random());
-
-
 
 const createBoard = () => {
     for(let i = 0; i < cardArray.length; i++){
@@ -88,26 +21,32 @@ const createBoard = () => {
 
 createBoard();
 
-
-
-
+// rules for the game
 const checkMatch = () => {
     cards = Array.from(document.querySelectorAll('img'));
     const optionOneId = cardsChosenIds[0];
     const optionTwoId = cardsChosenIds[1];
 
+    // in case the user click the same card
     if (optionOneId == optionTwoId){
-
         cards[optionOneId].setAttribute('src', 'images/image-blank.jpg')
         cards[optionTwoId].setAttribute('src', 'images/image-blank.jpg')
         alert('You have chosen the same image!')
-    } else if (cardsChosenNames[0] == cardsChosenNames[1]) {
+    }
+    
+    // when user finds equal cards
+    else if (cardsChosenNames[0] == cardsChosenNames[1]) {
         alert("congrats")
         cards[optionOneId].removeEventListener('click', flipCard)
         cards[optionTwoId].removeEventListener('click', flipCard)
+        cards[optionOneId].classList.add('dontFlip')
+        cards[optionTwoId].classList.add('dontFlip')
         cardsWon.push(cardsChosenNames[0])
         cardsWon.push(cardsChosenNames[1])
-    } else {
+    }
+    
+    // when user chooses wrong
+    else {
         cards[optionOneId].setAttribute('src', 'images/image-blank.jpg')
         cards[optionTwoId].setAttribute('src', 'images/image-blank.jpg')
         alert("not this time")
@@ -117,32 +56,34 @@ const checkMatch = () => {
     cardsChosenNames = []
     cardsChosenIds = []
 
-    cards.map(card => {
-        console.log(card)
-        // if (!cardsWon.includes(card.name)) {
-        //     card.addEventListener('click', flipCard)            
-        // }
+    // add the eventlisterners again, except the matched cards
+    cards.map(card => {        
+        if (!card.classList.contains('dontFlip')) {
+            card.addEventListener('click', flipCard)            
+        }
     })
 
+    // when user finishes the game
     if(cardsWon.length == cardArray.length){
         resultDisplay.textContent = "Congratulations, you found all pairs! :)"
     }
 }
 
-
+// for cards flips
 function flipCard() {
     const cardId = this.getAttribute('data-id')
    
     cardsChosenNames.push(cardArray[cardId].name)
     cardsChosenIds.push(cardId)
-
     this.setAttribute('src', cardArray[cardId].img)
 
     if(cardsChosenNames.length === 2) {
         cards = Array.from(document.querySelectorAll('img'));
-        cards.map(card => card.removeEventListener('click', flipCard))        
-        setTimeout(checkMatch, 500)
 
+        // removing eventlistener of all cards to prevent a third card flip while the message showed
+        cards.map(card => card.removeEventListener('click', flipCard))
+
+        setTimeout(checkMatch, 500)
     }    
 };
 
---
+++ cardArray.js
@@ -0,0 +1,66 @@
+export const cardArray = [
+    {
+        name: 'blouse1',
+        img: 'images/blouse1.jpg',
+    },
+    {
+        name: 'blouse2',
+        img: 'images/blouse2.jpg',
+    },
+    {
+        name: 'blouse3',
+        img: 'images/blouse3.jpg',
+    },
+    {
+        name: 'blouse4',
+        img: 'images/blouse4.jpg',
+    },
+    {
+        name: 'blouse5',
+        img: 'images/blouse5.jpg',
+    },
+    {
+        name: 'blouse6',
+        img: 'images/blouse6.jpg',
+    },
+    {
+        name: 'blouse7',
+        img: 'images/blouse7.jpg',
+    },
+    {
+        name: 'blouse8',
+        img: 'images/blouse8.jpg',
+    },
+    {
+        name: 'blouse1',
+        img: 'images/blouse1.jpg',
+    },
+    {
+        name: 'blouse2',
+        img: 'images/blouse2.jpg',
+    },
+    {
+        name: 'blouse3',
+        img: 'images/blouse3.jpg',
+    },
+    {
+        name: 'blouse4',
+        img: 'images/blouse4.jpg',
+    },
+    {
+        name: 'blouse5',
+        img: 'images/blouse5.jpg',
+    },
+    {
+        name: 'blouse6',
+        img: 'images/blouse6.jpg',
+    },
+    {
+        name: 'blouse7',
+        img: 'images/blouse7.jpg',
+    },
+    {
+        name: 'blouse8',
+        img: 'images/blouse8.jpg',
+    }
+];
